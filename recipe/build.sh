4abbd80ed594a809f2dbad886058b58161c7feab
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-04-11 19:52:20 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export LDFLAGS="${LDFLAGS} -llapack -lblas" 
-export LDFLAGS_LD="${LDFLAGS_LD} -llapack -lblas" 
+export LDFLAGS="${LDFLAGS} -llapack -lblas -lfftw3 -lfftw3f"
+export LDFLAGS_LD="${LDFLAGS_LD} -llapack -lblas -lfftw3 -lfftw3f" 
 ${PYTHON} -m pip install . -vv
