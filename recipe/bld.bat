1214489796d1858e83a53e29a2923263638dd5af
k-dominik <k-dominik@users.noreply.github.com>
2025-05-09 16:01:21 +0200

    Try building with flang on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,7 @@
 set FC=%BUILD_PREFIX%\Library\bin\flang.exe
 set "LDFLAGS=%LDFLAGS% -llapack -lblas"
 
+REM Needed for compilation with flang_win64:
+set LIB=%BUILD_PREFIX%\Library\lib;%LIB%
+
 %PYTHON% -m pip install . -vv
